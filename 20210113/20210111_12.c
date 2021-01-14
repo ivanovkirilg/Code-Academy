ec1736fe602ffcd33d63daff9c17ff92163b499c
Kiril Ivanov <76907506+ivanovkirilg@users.noreply.github.com>
2021-01-14 15:19:01 +0200

    Create 20210111_12.c

---
+++ 20210113/20210111_12.c
@@ -0,0 +1,42 @@
+/*  12. Напишете програма PrintPatterns, 
+    която отпечата следните текстови графики на екрана. 
+    Графиките една под друга: */
+
+#include <stdio.h>
+
+int main() 
+{
+    printf("\tA)\n");
+
+    printf("# # # # # # # # # # #\n");
+    printf("  # # # # # # # # #\n");
+    printf("    # # # # # # #\n");
+    printf("      # # # # #\n");
+    printf("        # # #\n");
+    printf("          #\n");
+    
+    printf("\n\tB)\n");
+
+    printf("          #\n");
+    printf("        # # #\n");
+    printf("      # # # # #\n");
+    printf("    # # # # # # #\n");
+    printf("  # # # # # # # # #\n");
+    printf("# # # # # # # # # # #\n");
+    
+    printf("\n\tC)\n");
+
+    printf("          #\n");
+    printf("        # # #\n");
+    printf("      # # # # #\n");
+    printf("    # # # # # # #\n");
+    printf("  # # # # # # # # #\n");
+    printf("# # # # # # # # # # #\n");
+    printf("  # # # # # # # # #\n");
+    printf("    # # # # # # #\n");
+    printf("      # # # # #\n");
+    printf("        # # #\n");
+    printf("          #\n");
+
+    return 0;
+}
