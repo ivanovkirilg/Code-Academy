092bceb8403475d90260766907cded0665738e32
Kiril Ivanov <ivanovkirilg@gmail.com>
2021-04-18 12:25:05 +0300

    decorators and modules

---
+++ Python/Modules/a.py
@@ -0,0 +1,8 @@
+import b
+import sys
+
+b.spam('alabala')
+
+print(b.text_to_list('alabala'))
+
+print(sys.path)
---
+++ Python/Modules/b.py
@@ -0,0 +1,6 @@
+def spam(text):
+    print(text, 'is spam!')
+
+def text_to_list(text):
+    new_list = [let for let in text]
+    return new_list
