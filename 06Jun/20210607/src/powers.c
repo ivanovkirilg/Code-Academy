c17db0133c5ce23f5f4ce317fee7b8fbbad0300a
Kiril Ivanov <ivanovkirilg@gmail.com>
2021-06-10 13:53:01 +0300

    task 11
    Co-authored-by: Nikolay Georgiev <georgievnikolay@users.noreply.github.com>

---
+++ 06Jun/20210607/src/powers.c
@@ -0,0 +1,41 @@
+//Разбийте число като сума от степени на двойката.
+#include <stdio.h>
+
+#define USAGE_STR "Usage: %s [positive integer]\n"
+
+int main(int argc, char *argv[])
+{
+    long number = 0;
+    int power = 0;
+
+    if (argc != 2) {
+        printf(USAGE_STR, argv[0]);
+        return 1; 
+    }
+
+    if (sscanf(argv[1], "%ld", &number) != 1) {
+        printf("%s is not an integer.\n", argv[1]);
+        printf(USAGE_STR, argv[0]);
+        return 1;
+    }
+
+    if (number < 0) {
+        printf("%ld is negative.\n", number);
+        printf(USAGE_STR, argv[0]);
+        return 1;
+    }
+    
+    while (number != 0){
+
+        if (number & 1u) {
+            printf("2^%d ", power);
+        }
+
+        number >>= 1;
+        power++;
+    }
+
+    putchar('\n');
+    
+    return 0;
+}
